94821e41c30511913a893c03731f878d35c24c3f
MizunoShinya <mizunos@unit-nus.develop.homes.co.jp>
2017-12-26 17:29:34 +0900

    nerd_grep

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
-ln -snf ~/dotfiles/.vimrc ~/.vimrc
-ln -snf ~/dotfiles/.bashrc ~/.bashrc
-
 mkdir -p ~/.vim/swp/
 mkdir -p ~/.vim/view/
+
+ln -snf ~/dotfiles/.bashrc ~/.bashrc
+ln -snf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -snf ~/dotfiles/.vimrc ~/.vimrc
+ln -snf ~/dotfiles/.vim/nerdtree_plugin ~/.vim/nerdtree_plugin
+
